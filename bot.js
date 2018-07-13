9d91c26e81d1bda2174427d0de9d384af81c17a4
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-07-13 21:26:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -139,7 +139,7 @@ client.on('message',async message => {
 ${thisMessage}\`\`\`
 =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 **وصف الكود**: ${boi}
-**تم النشر بواسطة**: {message.author.username}
+**تم النشر بواسطة**: {client.user}
 **المصدر / الشخص الذي صنع الكود**: ${boi2}`); 
           }
         }
