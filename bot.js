a13cd5d390db7154a4269b2b7dafa56ff2e14ca7
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-07-17 16:49:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -273,6 +273,17 @@ message.react("📩")
 }
 });
 
+client.on('message', message => {
+if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log(**⚠️ جاري اعادة تشغيل البوت... ⚠️**);
+        client.destroy();
+        child_process.fork(__dirname + "/bot.js");
+        console.log(\`\`تم اعادة تشغيل البوت\`\`);
+    }
+  
+  });
 
 
 client.login(process.env.BOT_TOKEN);
