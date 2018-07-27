be8e5dfca7e0a2e59bed073fab40d2d0eb03c643
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-07-27 17:35:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,6 +107,30 @@ client.on('message', message => {
   }
   });
 
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + '-^bc') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة :e_mail:: ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+
 const code = '-^';
 
 client.on('message',async message => {
