4e9c53ad376216f0925bdb2810d2809adf34994f
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-08-19 14:52:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,8 +70,9 @@ let Jackeo = new Discord.RichEmbed()
     .setColor("RANDOM")
     //.setAuthor(message.author.username,message.author.avatarURL)
     //.setThumbnail(message.author.avatarURL)
+    //.setFooter("Alpha Codes. ")
+    .addField("**"+anarg+"**")
     .setFooter("Alpha Codes. ")
-    .addField("**• الرسالة**" "**"+anarg+"**")
 var prefix = "-^";
 if(command === `${prefix}send`) {
     if(toSend.bot) return message.reply("** لا يمكنك ارسال رسالة لبوت** :robot: ");
