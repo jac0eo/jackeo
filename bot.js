5d0b3464f13a38346af5270c98c58c2c4d242953
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-08-21 00:26:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -112,21 +112,6 @@ client.on('voiceStateUpdate', (old, now) => {
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","Members 🎮"));//here 
     });
-client.on('guildMemberAdd', member => {
-     const welcomer =  member.guild.channels.find('name', 'wlc');
-    if(!welcomer) return;
-      if(welcomer) {
-         moment.locale('ar-ly');
-         var m = member.user;
-        let yumz = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(m.avatarURL)
-        .setAuthor(m.username,m.avatarURL)
-        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
-
-         .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
-     welcomer.send({embed:yumz});
-
 
 
 
