9b5f6686b7921382bc6031c2e026d2065964ef00
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-08-21 03:02:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,8 +109,8 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('353991756519309333');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: [${currentSize}]`);
-  if (currentSize !== size) channel.setName(`Voice Online: [${currentSize}]`);
+  if (!size) return channel.setName(`『الاعضاء بالرومات الصوتية ↩ ${currentSize}』`);
+  if (currentSize !== size) channel.setName(`『الاعضاء بالرومات الصوتية ↩ ${currentSize}』`);
 });
 
 
