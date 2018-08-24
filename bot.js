97444cd988d351c3f40c51dff44023427cd9fd68
ali0iraq <30835872+ali0iraq@users.noreply.github.com>
2018-08-24 19:00:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -763,19 +763,32 @@ client.on('message', function(message) {
    }
 }); */
 
-let profile = JSON.parse(fs.readFileSync("./profile.json", "utf8"))
-client.on("message", message => {
-  if (message.author.bot) return;
- if(!message.channel.guild)return;
-  if (!profile[message.author.id]) profile[message.author.id] = {
-    tite: 'JACKEO',
-    rep: 0,
-   reps: 'NOT YET',
-   lastDaily:'Not Collected',
-    level: 0,
-    points: 0,
-    credits: 10500,
-  };
+fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
+    if (err) console.error(err);
+    })
+    client.on("message", (message) => {
+        if (!message.channel.type == "text") return;
+        if (message.content.startsWith(prefix + "تبرع")) {
+            var men = message.mentions.members.first();
+            if (!men) return message.reply("منشن العضو")
+            if (!message.content.split(" ")[2]) return message.reply("اكتب القيمة المراد تبرعها");
+            if (message.content.split(" ")[2] <0 || message.content.split(" ")[2] > 10000000000000000) return message.reply("اكتب القيمة المراد تبرعها");
+                message.channel.send("تم تحويل المبلغ بنجاح");
+                if (!profile[m.id]) {
+                    profile[m.id] = {
+                        tite: '.',
+        rep: 0,
+        reps: 'NOT YET',
+        lastDaily:'Not Collected',
+        level: 0,
+        points: 0,
+        credits: 15005,
+                    }
+                }
+                profile[m.id].credits+=message.content.split(" ")[2];
+                profile[message.author.id].credits-=message.content.split(" ")[2];
+        }
+    });
 fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
 if (err) console.error(err);
 })
@@ -805,10 +818,10 @@ if(message.content.startsWith(prefix + "daily")) {
     profile[message.author.id].lastDaily = moment().format('day')
       let args = message.content.split(" ").slice(1);
     let textxt = args.slice(0).join("");
-let daily = Math.floor(Math.random() * 130) + 350;
+let daily = Math.floor(Math.random() * 100) + 5500;
     profile[message.author.id].credits += daily;
     fs.writeFile("./profile.json", JSON.stringify(profile), function(err) {if(err) console.error(err)});
-     message.channel.send(`**${message.author.username} you collect your \`${daily}\` :dollar: daily pounds**`)
+     message.channel.send(`**${message.author.username} you collect your ${daily}  :yen: daily pounds**`)
 } else {
     message.channel.send(`**:atm: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
 }
